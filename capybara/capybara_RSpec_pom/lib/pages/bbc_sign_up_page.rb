3c4aa69544db5486f893e45216541cb3021312d2
Tech-A29 <Tech-A29@Tech-A29s-Air.lan>
2017-12-19 19:15:14 +0000

    signup success test ok

--- capybara/capybara_RSpec_pom/lib/pages/bbc_sign_up_page.rb
+++ capybara/capybara_RSpec_pom/lib/pages/bbc_sign_up_page.rb
@@ -8,6 +8,11 @@ class BbcSignUpPage
   YEAR_INPUT_FIELD_ID = "year-input"
   SUBMIT_BUTTON_ID = "submit-button"
   DATE_ERROR_MESSAGE_ID = "form-message-dateOfBirth"
+  EMAIL_INPUT_FIELD_ID = "email"
+  PASSWORD_INPUT_FIELD_ID = "password"
+  POSTCODE_INPUT_FIELD_ID = "postcode"
+  GENDER_INPUT_FIELD_ID = "gender-input"
+  OPTIN_CHOICE_ID = "#marketingOptIn"
   EMAIL_ERROR_MESSAGE_ID = "form-message-email"
   PASSWORD_ERROR_MESSAGE_ID = "form-message-password"
   POSTCODE_ERROR_MESSAGE_ID = "form-message-postcode"
@@ -35,6 +40,26 @@ class BbcSignUpPage
     find(:id, DATE_ERROR_MESSAGE_ID).text
   end
 
+  def input_email(email)
+    fill_in(EMAIL_INPUT_FIELD_ID, with: email)
+  end
+
+  def input_password(password)
+    fill_in(PASSWORD_INPUT_FIELD_ID, with: password)
+  end
+
+  def input_postcode(postcode)
+    fill_in(POSTCODE_INPUT_FIELD_ID, with: postcode)
+  end
+
+  def select_gender(gender)
+    select(gender, from: GENDER_INPUT_FIELD_ID)
+  end
+
+  def click_opt(opt)
+    find(:xpath, ".//label[@for='#{opt}']/div/div").click
+  end
+
   def email_error_text
     find(:id, EMAIL_ERROR_MESSAGE_ID).text
   end
@@ -53,5 +78,9 @@ class BbcSignUpPage
 
   def optin_error_text
     find(:id, OPTIN_ERROR_MESSAGE_ID).text
+  end
+
+  def signup_success
+    find(:xpath, ".//h1/span").text
   end
 end
--- capybara/capybara_RSpec_pom/spec/bbc_signup_success_spec.rb
+++ capybara/capybara_RSpec_pom/spec/bbc_signup_success_spec.rb
@@ -1,1 +1,33 @@
+require 'spec_helper'
 
+describe 'Correct details input' do
+
+  context 'it should correctly signup when correct details are input' do
+
+    before(:each) do
+      @bbc_site = BbcSite.new
+      @home = @bbc_site.bbc_homepage
+      @signin = @bbc_site.bbc_sign_in_page
+      @signup = @bbc_site.bbc_sign_up_page
+    end
+
+    it "should signup successfully" do
+      @home.visit_home_page
+      @home.click_sign_in_link
+      @signin.register_link
+      @signup.input_day("2")
+      @signup.input_month("2")
+      @signup.input_year("1992")
+      @signup.submit
+      @signup.input_email("kalok38745@gmail.com")
+      @signup.input_password("kalok941")
+      @signup.input_postcode("SL6 2BL")
+      @signup.select_gender("Male")
+      @signup.click_opt("optOut")
+      @signup.submit
+
+      sleep 5
+      expect(@signup.signup_success).to eq "Thanks. You're now signed in."
+    end
+  end
+end
